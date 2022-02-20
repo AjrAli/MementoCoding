ed0468d9b520ce1dd017731e7d923f90cfdf6216
AidenPrime <40406161+AjrAli@users.noreply.github.com>
2022-02-20 21:37:24 +0100

    beginning unit test project

---
+++ SchoolProject.Management.Api.Tests/Controllers/StudentControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolProject.Management.Api.Controllers;
+namespace SchoolProject.Management.Api.Tests.Controllers
+{
+    [TestClass]
+    public class StudentControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
