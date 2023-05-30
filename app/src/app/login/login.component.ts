7f5c416647276017f68573dee9383c0d2ff5aaa6
AidenPrime <ali.itbxl@gmail.com>
2023-05-30 19:24:06 +0200

    UI : adding approriate error message for login + TODO adding AutoMapper like for Angular object

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../services/authentification/authentication.service';
 import { Router } from '@angular/router';
-
+import { ErrorResponse, harmonizeErrorResponse } from '../dto/error/error-response';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,22 +10,29 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   username: string = '';
   password: string = '';
-
+  errorMessage!: ErrorResponse;
   constructor(private authService: AuthenticationService, private router: Router) { }
 
   onSubmit() {
-    this.authService.authenticate(this.username, this.password).subscribe(
-      (response: any) => {
-        if (response.token) {
-          this.authService.setToken(response.token);
+    this.authService.authenticate(this.username, this.password).subscribe({
+      next: (r) => {
+        if (r.token) {
+          this.authService.setToken(r.token);
           this.router.navigate(['/schools']);
         } else {
           alert('Invalid credentials');
         }
       },
-      (error: any) => {
-        alert('Error: ' + error.message);
-      }
-    );
+      error: (e) => {
+        if (e.status === 400 || e.status === 404) {
+          this.errorMessage = harmonizeErrorResponse(e.error as ErrorResponse);
+          console.error(this.errorMessage);
+        } else {
+          console.error('Error:', e);
+          alert('Error: ' + e.message);
+        }
+      },
+      complete: () => console.info('complete')
+    });
   }
 }
