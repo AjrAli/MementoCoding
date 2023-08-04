fd4f5e9fb58e9d6be51936d3114de3ca060b4c59
AidenPrime <ali.itbxl@gmail.com>
2023-08-04 19:09:46 +0200

    UI: fix unit test

--- app/src/app/services/modal/modal.service.spec.ts
+++ app/src/app/services/modal/modal.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ModalService } from './modal.service';
+import { ToastService } from '../message-popup/toast.service';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('ModalService', () => {
   let service: ModalService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ToastrModule.forRoot(), // Only import ToastrModule once
+      ],
+      providers: [ToastService]
+    });
     service = TestBed.inject(ModalService);
   });
 
